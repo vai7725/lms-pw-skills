c7aeadd8ea29f03431a046789abbe629286582ef
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-09-16 14:05:47 +0530

    setup complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+
 import './App.css';
+import { Toaster } from 'react-hot-toast';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <RouterProvider router={router} />;
+    </>
+  );
 }
 
 export default App;
